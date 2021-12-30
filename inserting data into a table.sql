d30526a495dd0eb5de5f7f8a4fb842ffacbedcb7
Sara Kubik <27822918+sarakubik@users.noreply.github.com>
2021-12-30 08:56:09 -0600

    Add files via upload

---
+++ inserting data into a table.sql
@@ -0,0 +1,22 @@
+select *
+from Customer2
+-- insert data
+
+insert into dbo.Customer2 (FirstName, LastName, Age)
+ values ('Joey', 'Blue', 43);
+
+ insert into dbo.Customer2 (FirstName, LastName, Age)
+ values ('Haey', 'Jones', 52);
+
+ insert into dbo.Customer2 (FirstName, LastName, Age)
+ values ('Dude', 'Schemit', 45);
+
+ insert into dbo.Customer2 (FirstName, LastName, Age)
+ values ('Sally', 'White', 48);
+
+ insert into dbo.Customer2 (FirstName, LastName, Age)
+ values ('Cal', 'Lognerd', 55);
+
+
+ insert into dbo.Customer2 (FirstName, LastName, Age)
+ values ('Joey', 'Nane', 56);
